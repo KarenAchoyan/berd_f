c6a7f5d59d04042b00fdb4867c4dd8efb677dca5
Karen <karenachoyan8@gmail.com>
2025-03-16 19:45:50 +0400

    Delete favicon

--- app/page.js
+++ app/page.js
@@ -1,39 +1,47 @@
 import Image from "next/image";
 import Performance from "@/components/performance/performance";
 import Tickets from "@/components/tickets/tickets";
-import {Suspense} from "react";
-import {VideoProvider} from "@/providers/VideoProvider";
-import {VideoComponent} from "@/components/banner/banner";
+import { Suspense } from "react";
+import { VideoComponent } from "@/components/banner/banner";
 import Galleries from "@/components/galleries/galleries";
-import {MainProvider} from "@/providers/MainProvider";
+import { MainProvider } from "@/providers/MainProvider";
 
+// Async function to fetch speeches data
 async function getSpeeches() {
     const res = await fetch('https://berd.dahk.am/api/speeches');
+    if (!res.ok) {
+        throw new Error('Failed to fetch speeches');
+    }
     return res.json();
 }
 
+// Async function to fetch galleries data
 async function getGalleries() {
     const res = await fetch('https://berd.dahk.am/api/galleries');
+    if (!res.ok) {
+        throw new Error('Failed to fetch galleries');
+    }
     return res.json();
 }
 
 export default async function Home() {
+    // Fetching data directly inside the component
     const speeches = await getSpeeches();
     const galleries = await getGalleries();
     const videoUrl = "./berd_banner.mp4";
+
     return (
         <>
-            <MainProvider value={{videoUrl, speeches, galleries}}>
+            <MainProvider value={{ videoUrl, speeches, galleries }}>
                 <div className="relative w-full h-[400px] sm:h-[600px] md:h-[800px] overflow-hidden">
                     <Suspense fallback={<p>Loading video...</p>}>
-                        <VideoComponent/>
+                        <VideoComponent />
                     </Suspense>
                 </div>
                 <Performance />
-                <Tickets/>
-                <Galleries/>
+                <Tickets />
+                <Galleries />
             </MainProvider>
         </>
     );
 }
-
